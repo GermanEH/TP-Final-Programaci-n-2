cc40365b3d7e649c5ea926e8b037036cddc5f063
Mauri <tognocchitomas@gmail.com>
2025-10-02 00:15:01 -0300

    Agrego clase Profesor y Pregunta para poder avanzar con clase Examen

--- App/src/Clases/Examen.java
+++ App/src/Clases/Examen.java
@@ -1,4 +1,20 @@
 package Clases;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Examen {
+    List<Pregunta> preguntasJava = new ArrayList<>();
+    List<Pregunta> preguntasSql = new ArrayList<>();
+    int respuestasCorrectas = 0 ;
+
+    public void agregarPreguntaJava (Pregunta p){
+         preguntasJava.add(p);
+    }
+
+    public void agregarPreguntaSql (Pregunta p){
+        preguntasSql.add(p);
+    }
+
+
 }
