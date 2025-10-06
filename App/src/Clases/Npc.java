181e370734e6124bd4219f310433595104943092
technofriki <jack_mdq97@hotmail.com>
2025-10-06 18:41:18 -0300

    commit de prueba

---
+++ App/src/Clases/Npc.java
@@ -0,0 +1,5 @@
+package Clases;
+
+public class Npc extends Personaje{
+
+}
--- App/src/Clases/Personaje.java
+++ App/src/Clases/Personaje.java
@@ -1,4 +1,42 @@
 package Clases;
 
-public class Personaje {
+public abstract class Personaje {
+    private String nombre;
+    private int posX;
+    private int posY;
+
+    public Personaje() {
+    }
+
+    public Personaje(String nombre, int posX, int posY) {
+        this.nombre = nombre;
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+    public abstract void interactuar (Jugador jugador);
 }
