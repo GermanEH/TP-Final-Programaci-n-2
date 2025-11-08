c34ac1d3aa664d53fdfcbeca93d9b8719bc77d2b
Mauri <tognocchitomas@gmail.com>
2025-11-08 13:46:28 -0300

    Modifico la clase JSONUtiles para tener metodos mas genericos

--- App/src/Clases/JSONUtiles.java
+++ App/src/Clases/JSONUtiles.java
@@ -6,31 +6,45 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.json.JSONTokener;
 
 public class JSONUtiles {
 
-    public static void grabar(JSONArray array) {
-        try {
-            FileWriter file = new FileWriter("concesionaria.json");
-            file.write(array.toString());
-            file.flush();
-            file.close();
+    /// METODO GENERICO PARA GRABAR JSONARRAY
+    public static void grabar(String archivo, JSONArray array) {
+        try (FileWriter file = new FileWriter(archivo)) {
+            file.write(array.toString(4)); // formato mas lindo con 4, agrego catch
+
         } catch (IOException e) {
-
             e.printStackTrace();
+        } catch (JSONException e) { // para capturar error del formato
+            throw new RuntimeException(e);
         }
     }
 
+    /// METODO GENERICO PARA GRABAR JSONOBJECT
+    public static void grabar(String archivo, JSONObject obj) {
+        try (FileWriter file = new FileWriter(archivo)) {
+            file.write(obj.toString(4));
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /// METODO PARA LEER JSON
     public static JSONTokener leer(String archivo) {
-        JSONTokener tokener = null;
-
         try {
-            tokener = new JSONTokener(new FileReader(archivo));
+            return new JSONTokener(new FileReader(archivo));
+
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            System.out.println("ERROR: ARCHIVO NO ENCONTRADO");
+            return null;
         }
-        return tokener;
     }
+
 }
